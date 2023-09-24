22d6ffe5943578c42469c8b44a1c4e6429581b9e
rcrosbourne <rcrosbourne@gmail.com>
2023-09-24 03:51:49 -0500

    configure octane phpstan laravel pint

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,16 @@ export default defineConfig({
         }),
         react(),
     ],
+     server: {
+        hmr: {
+            protocol: 'wss',
+            host: 'laravel-starter-template.ddev.site',
+        },
+        host: '0.0.0.0',
+        strictPort: true,
+        // dev server port
+        port: 3000,
+        // SSR server port
+        // port: 13714,
+    },
 });
